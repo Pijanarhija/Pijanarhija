d1e410f498856a6c661648aa055babc08f03f913
Nikola <nikola211223232529321@gmail.com>
2025-09-14 19:36:08 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export", // Enables static site generation
-	trailingSlash: true, // Ensures compatibility with Netlify's routing
+	trailingSlash: true, // Ensures compatibility with Netlify routing
 	images: {
-		unoptimized: true, // Disables Next.js image optimization for static export
+		unoptimized: true, // Disables image optimization for static export
 	},
 };
 
