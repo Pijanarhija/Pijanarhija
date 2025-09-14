da247433d0b726b205d3352fb5351997594c8df0
Nikola <nikola211223232529321@gmail.com>
2025-09-14 17:59:30 +0200

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import FaqSection from "@/components/FaqSection";
 import HeroSection from "@/components/HeroSection";
 import HowToPlaySection from "@/components/HowToPlaySection";
 import InfoSection from "@/components/InfoSection";
-import SponsorSection from "@/components/SponsorSection";
 import WhatYouGet from "@/components/WhatYouGet";
 import aboutPattern from "../public/assets/about-pattern.webp";
 import cardsPattern from "../public/assets/cards-pattern.webp";
@@ -17,66 +16,66 @@ import sponsorPattern from "../public/assets/sponsor-pattern.webp";
 import whatyougetPattern from "../public/assets/whatyouget-pattern.webp";
 
 export default function Home() {
-  return (
-    <main>
-      <HeroSection />
-      <AboutSection />
-      <section className="bg-accent">
-        <Image
-          src={aboutPattern}
-          alt="paper pattern"
-          className="h-auto w-full max-sm:w-[200%]"
-        />
-      </section>
-      <CharactersSection />
-      <section className="bg-accent">
-        <Image
-          src={cardsPattern}
-          alt="paper pattern"
-          className="h-auto w-full max-sm:w-[400%]"
-        />
-      </section>
-      <HowToPlaySection />
-      <section className="bg-accent">
-        <Image
-          src={howtoplayPattern}
-          alt="paper pattern"
-          className="h-auto w-full max-sm:w-[200%]"
-        />
-      </section>
-      <WhatYouGet />
-      <section className="bg-accent">
-        <Image
-          src={whatyougetPattern}
-          alt="paper pattern"
-          className="h-auto w-full max-sm:w-[400%]"
-        />
-      </section>
-      <InfoSection />
-      <section className="bg-accent">
-        <Image
-          src={infoPattern}
-          alt="paper pattern"
-          className="h-auto w-full max-sm:w-[200%]"
-        />
-      </section>
-      <ContactSection />
-      <section className="bg-accent">
-        <Image
-          src={contactPattern}
-          alt="paper pattern"
-          className="h-auto w-full max-sm:w-[400%]"
-        />
-      </section>
-      <FaqSection />
-      {/* <SponsorSection /> */}
-      <section className="bg-accent">
-        <Image
-          src={sponsorPattern}
-          alt="paper pattern"
-          className="h-auto w-full max-sm:w-[400%]"
-        />
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<HeroSection />
+			<AboutSection />
+			<section className="bg-accent">
+				<Image
+					src={aboutPattern}
+					alt="paper pattern"
+					className="h-auto w-full max-sm:w-[200%]"
+				/>
+			</section>
+			<CharactersSection />
+			<section className="bg-accent">
+				<Image
+					src={cardsPattern}
+					alt="paper pattern"
+					className="h-auto w-full max-sm:w-[400%]"
+				/>
+			</section>
+			<HowToPlaySection />
+			<section className="bg-accent">
+				<Image
+					src={howtoplayPattern}
+					alt="paper pattern"
+					className="h-auto w-full max-sm:w-[200%]"
+				/>
+			</section>
+			<WhatYouGet />
+			<section className="bg-accent">
+				<Image
+					src={whatyougetPattern}
+					alt="paper pattern"
+					className="h-auto w-full max-sm:w-[400%]"
+				/>
+			</section>
+			<InfoSection />
+			<section className="bg-accent">
+				<Image
+					src={infoPattern}
+					alt="paper pattern"
+					className="h-auto w-full max-sm:w-[200%]"
+				/>
+			</section>
+			<ContactSection />
+			<section className="bg-accent">
+				<Image
+					src={contactPattern}
+					alt="paper pattern"
+					className="h-auto w-full max-sm:w-[400%]"
+				/>
+			</section>
+			<FaqSection />
+			{/* <SponsorSection /> */}
+			<section className="bg-accent">
+				<Image
+					src={sponsorPattern}
+					alt="paper pattern"
+					className="h-auto w-full max-sm:w-[400%]"
+				/>
+			</section>
+		</main>
+	);
 }
